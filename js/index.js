e8dc318369892cde3ecd427b597bdbb43cfda375
minamirza <minamirzeva@gmail.com>
2025-03-12 21:29:51 +0400

    script

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
+const DATA = 
+const content = document.getElementById("content")
+
+
 fetch("https://restcountries.com/v3.1/all")
-  .then((res) => res.json())
-  .then((melumat) => console.log(melumat));
+    .then(res => res.json())
+    .then(melumat => {DATA.push(...melumat)})
+
+
+function show(){
+    DATA.forEach(    item => {
+        content.innerHTML += `
+             <article class="flex flex-col dark:bg-gray-50">
+                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+                    <img alt="" class="object-cover w-full h-52 dark:bg-gray-500"
+                        src="https://source.unsplash.com/200x200/?fashion?1">
+                </a>
+                <div class="flex flex-col flex-1 p-6">
+                    <a rel="noopener noreferrer" href="#"
+                        aria-label="Te nulla oportere reprimique his dolorum"></a>
+                    <a rel="noopener noreferrer" href="#"
+                        class="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">Convenire</a>
+                    <h3 class="flex-1 py-2 text-lg font-semibold leading-snug">Te nulla oportere reprimique his
+                        dolorum</h3>
+                    <div class="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-600">
+                        <span>June 1, 2020</span>
+                        <span>2.1K views</span>
+                    </div>
+                </div>
+            </article>
+        `
+    }     )
+}
+show()
